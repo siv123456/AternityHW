0fdce3ab71b99a4b3b6095fcee770ba133b5746e
sivan b <sivbfreelancer@gmail.com>
2015-10-11 12:29:55 +0300

    more buttons and functionality
    1. added the edit/remove buttons to the table.
    2. made sure for each button i open the dialog with the right
    configuration i.e header, only right buttons enabled and shown.

--- apache-tomcat-8.0.26-windows-x64/webapps/ROOT/mainModule.js
+++ apache-tomcat-8.0.26-windows-x64/webapps/ROOT/mainModule.js
@@ -11,11 +11,11 @@ myApp.factory('roles',function($http){
 
     // GET
     self.get = roles.get;
-    roles.get = function(callback) {
+    roles.get = function(callbackf) {
         $http.get("http://localhost:8080/api/roles")
             .then(function successCallback(response){
                 roles.list = response.data.roles;
-                callback(response.data.privileges);
+                callbackf(response.data.privileges);
 
             }, function errorCallback(response){
                 alert(response.statusText);
@@ -26,18 +26,96 @@ myApp.factory('roles',function($http){
 
 });
 
-myApp.controller('MainCtrl', function($scope, ngDialog, roles) {
+myApp.factory('dialogVars', function(){
+    var self = this;
+    var dVars = {};
+    dVars.values = {};
+
+    self.set = dVars.set;
+    dVars.set = function(dialogType){
+        switch(dialogType){
+            case 'view':
+                dVars.values = {
+                    disableEditBtn : true,
+                    disableRemoveBtn : true,
+                    Header : 'View Role'
+                };
+                break;
+            case 'edit':
+                dVars.values = {
+                    disableEditBtn : false,
+                    disableRemoveBtn : true,
+                    Header : 'Edit Role'
+                };
+                break;
+            case 'remove':
+                dVars.values = {
+                    disableEditBtn : true,
+                    disableRemoveBtn : false,
+                    Header : 'Remove Role'
+                };
+                break;
+            default:
+
+        }
+    };
+
+
+    return dVars
+});
+
+myApp.controller('MainCtrl', function($scope, ngDialog, roles, dialogVars) {
     var self = this;
     self.roles = roles;
     self.roles.get();
+    self.dialogVars = dialogVars;
 
-    self.expandDialog = function(id){
-        self.expandRoleID = id;
+    // Expand Role row into dialog
+    self.expandDialog = function(id,dialogType){
+        self.roleID = id;
+        self.dialogVars.set(dialogType);
+
         ngDialog.open({
             template: 'dialogTemplate.html',
             closeByEscape: false,
             closeByDocument: false,
             showClose: false,
+            scope: $scope,
+            className: 'ngdialog-theme-default'
+        }).then(
+            function(value) {
+                if (value == 'close') {
+                    ngdialog.close();
+                }
+            }
+        );
+    };
+
+    // Expand row to edit role
+    self.editDialog = function(id){
+        self.roleID = id;
+        self.editBtnDisable = false;
+        self.removeBtnDisable = true;
+        self.dialogHeader = "Role To Edit";
+        ngDialog.open({
+            template: 'dialogTemplate.html',
+            closeByEscape: false,
+            closeByDocument: false,
+            scope: $scope,
+            className: 'ngdialog-theme-default'
+        });
+    }
+
+    // Expand row to remove role
+    self.removeDialog = function(id){
+        self.roleID = id;
+        self.editBtnDisable = true;
+        self.removeBtnDisable = false;
+        self.dialogHeader = "Role To Remove";
+        ngDialog.open({
+            template: 'dialogTemplate.html',
+            closeByEscape: false,
+            closeByDocument: false,
             scope: $scope,
             className: 'ngdialog-theme-default'
         });
