79a070db48ae1eae89862b2b206d0083ffc94bae
sivan b <sivbfreelancer@gmail.com>
2015-10-12 22:26:15 +0300

    lots of improvements
    1. added icons to buttons (view, remove, edit, create)
    2. centered the dialog when its opened
    3. view ,create ,edit, remove all functioning.

--- apache-tomcat-8.0.26-windows-x64/webapps/ROOT/mainModule.js
+++ apache-tomcat-8.0.26-windows-x64/webapps/ROOT/mainModule.js
@@ -32,6 +32,38 @@ myApp.factory('roles',function($http){
             });
     };
 
+    // CREATE
+    self.create = roles.create;
+    roles.create = function(callback, createObj){
+        var newRole = {
+            "name":createObj.name,
+            "description":createObj.description,
+            "privileges":createObj.privileges.split(",")
+        };
+        $http.post("http://localhost:8080/api/roles/create",JSON.stringify(newRole))
+            .then(function successCallback(response){
+                callback(response);
+            },function errorCallback(response){
+                callback(response);
+            });
+    };
+
+    // UPDATE
+    self.update = roles.update;
+    roles.update = function(callback, id, updateObj){
+        var newRole = {
+            "name":updateObj.name,
+            "description":updateObj.description,
+            "privileges":updateObj.privileges.split(",")
+        };
+        $http.post("http://localhost:8080/api/roles/"+id+"/update",JSON.stringify(newRole))
+            .then(function successCallback(response){
+                callback(response);
+            },function errorCallback(response){
+                callback(response);
+            });
+    };
+
     return roles;
 
 });
@@ -43,36 +75,65 @@ myApp.factory('dialogServices', function(){
     dVars.values = {};
 
     self.set = dVars.set;
-    dVars.set = function(dialogType){
-        dVars.values['statusText'] = "";
+    dVars.set = function(dialogType, roleLineObj){
+
         switch(dialogType){
             case 'view':
                 dVars.values = {
-                    disableEditBtn : true,
-                    disableRemoveBtn : true,
+                    disableNameInput: true,
+                    disableDescriptionInput: true,
+                    disablePrivilegesInput: true,
+                    hideEditBtn : true,
+                    hideCreateBtn: true,
+                    hideRemoveBtn : true,
                     Header : 'View Role',
                     hideStatus: false
                 };
                 break;
             case 'edit':
                 dVars.values = {
-                    disableEditBtn : false,
-                    disableRemoveBtn : true,
+                    disableNameInput: false,
+                    disableDescriptionInput: false,
+                    disablePrivilegesInput: false,
+                    hideEditBtn : false,
+                    hideCreateBtn: true,
+                    hideRemoveBtn : true,
                     Header : 'Edit Role',
                     hideStatus: true
                 };
                 break;
             case 'remove':
                 dVars.values = {
-                    disableEditBtn : true,
-                    disableRemoveBtn : false,
+                    disableNameInput: true,
+                    disableDescriptionInput: true,
+                    disablePrivilegesInput: true,
+                    hideEditBtn : true,
+                    hideCreateBtn: true,
+                    hideRemoveBtn : false,
+                    Header : 'Remove Role',
+                    hideStatus: false
+                };
+                break;
+            case 'create':
+                dVars.values = {
+                    disableNameInput: false,
+                    disableDescriptionInput: false,
+                    disablePrivilegesInput: false,
+                    hideEditBtn : true,
+                    hideCreateBtn: false,
+                    hideRemoveBtn : true,
                     Header : 'Remove Role',
                     hideStatus: false
                 };
                 break;
             default:
 
+
         }
+        dVars.values['statusText'] = "";
+        dVars.values['name'] = roleLineObj['name'];
+        dVars.values['description'] = roleLineObj['description'];
+        dVars.values['privileges'] = roleLineObj['privileges'];
     };
 
     self.removeRole = dVars.removeRole;
@@ -95,7 +156,16 @@ myApp.controller('MainCtrl', function($scope, ngDialog, roles, dialogServices) {
     // Expand Role row into dialog
     self.openDialog = function(id,dialogType){
         self.roleID = id;
-        self.dialogServices.set(dialogType);
+        if (id > 0) {
+            self.dialogServices.set(dialogType, self.roles.list[id]);
+        } else {
+            self.dialogServices.set(dialogType, {
+                name: "please insert name",
+                description: "Please write a description",
+                privileges: "please choose privileges"
+            });
+        }
+
         ngDialog.closeAll();
         ngDialog.open({
             template: 'dTemplate.html',
@@ -108,12 +178,7 @@ myApp.controller('MainCtrl', function($scope, ngDialog, roles, dialogServices) {
         })
     };
 
-    // create init values
-    self.createObj = {};
-    self.createObj.name = 'secure444';
-    self.createObj.desc = 'Description..';
-    self.createObj.priv = 'View Data,View Users,Manage Users,View Roles';
-
+    // run delete role
     self.remove = function(id){
         //self.dialogServices.removeRole(id);
         ngDialog.closeAll();
@@ -126,4 +191,25 @@ myApp.controller('MainCtrl', function($scope, ngDialog, roles, dialogServices) {
         },id);
     };
 
+    // run the create role
+    self.create = function() {
+        self.roles.create(function (res) {
+            if (res.statusText == "OK") {
+                self.roles.get()
+            } else {
+                alert("Role creation problem: " + res.data);
+            }
+        }, self.dialogServices.values);
+    };
+
+    // run update role
+    self.update = function(id) {
+        self.roles.update(function (res) {
+            if (res.statusText == "OK") {
+                self.roles.get()
+            } else {
+                alert("Role creation problem: " + res.data);
+            }
+        }, id, self.dialogServices.values);
+    };
 });
